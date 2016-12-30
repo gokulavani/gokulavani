22f71c15dc8e7fae0b034a4759982f146be29c03
gokulavani <dhilipkumarvv@gmail.com>
2016-12-30 12:44:47 +0530

    Add files via upload

---
+++ numberww.c
@@ -0,0 +1,16 @@
+#include<stdio.h>
+#include<conio.h>
+int main()
+{
+
+    int sum=0,i,n;
+    printf("enter the value :");
+    scanf("%d",&n);
+    for(i=0;i<=n;i++)
+    {
+
+        sum=sum+i;
+
+    }printf("the value are:%d",sum);
+    return 0;
+}
