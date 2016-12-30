9bcf76a6fe043612a725f17d5a4b4318bfc449e9
gokulavani <dhilipkumarvv@gmail.com>
2016-12-30 09:19:28 +0530

    Add files via upload

---
+++ evenor odd.c
@@ -0,0 +1,16 @@
+#include<stdio.h>
+#include<conio.h>
+int main()
+{
+int num;
+printf("enter the number:");
+scanf("%d",&num);
+if(num%2==0)
+{
+printf("the values is even number:%d",num);
+}
+else{
+    printf("the values is odd number:%d",num);
+
+}
+return 0;}
