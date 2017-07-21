30cd27fa68709f2c88369c70f153e58dce3ff71a
gokulavani <dhilipkumarvv@gmail.com>
2017-07-21 14:32:52 +0530

    Add files via upload

---
+++ SplitUp.java
@@ -0,0 +1,17 @@
+import java.util.*;
+public class SplitUp {
+
+	
+	public static void main(String[] args) {
+Scanner s=new Scanner(System.in);
+String str=s.nextLine();
+
+String a[]=str.split(" ");
+for(int i=a.length-1;i>=0;i--)
+{
+	System.out.print(a[i]+" ");
+}
+s.close();
+	}
+
+}
