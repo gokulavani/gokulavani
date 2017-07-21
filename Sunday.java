32113d023192f982457000298c493e91a96f1426
gokulavani <dhilipkumarvv@gmail.com>
2017-07-21 17:31:36 +0530

    Add files via upload

---
+++ Sunday.java
@@ -0,0 +1,20 @@
+import java.util.*;
+public class Sunday {
+
+	
+	public static void main(String[] args) {
+		Scanner s=new Scanner(System.in);
+		String sv=s.next();
+		if(sv.equals("Sunday"))
+		{
+			System.out.println("false");
+		}
+		else
+		{
+			System.out.println("true");
+
+		}
+		s.close();
+	}
+
+}
