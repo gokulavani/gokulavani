ead2de2b684ea289e86870df48dc0fd91dda09d3
gokulavani <dhilipkumarvv@gmail.com>
2017-07-22 12:45:58 +0530

    Add files via upload

---
+++ LeapYera.java
@@ -0,0 +1,20 @@
+import java.util.*;
+public class LeapYera {
+
+	public static void main(String[] args) {
+		Scanner s=new Scanner(System.in);
+		int year=s.nextInt();
+		int count=0;
+		if(year%4==0)
+			for(int i=0;i<20;i++)
+			{
+				year+=4;
+				count++;
+				System.out.println(year);
+				System.out.println(count);
+
+			}
+		  s.close();                                  
+	}
+
+}
