6b09a81d319a1b37e370f3fa9cc48fdf7170574d
gokulavani <dhilipkumarvv@gmail.com>
2017-08-04 15:54:34 +0530

    Add files via upload

---
+++ program.java
@@ -0,0 +1,27 @@
+package Pro;
+import java.util.*;
+public class program {
+	public static void main(String [] argms)
+	{
+		Scanner s=new Scanner(System.in);
+		int n=s.nextInt();
+		int ns=s.nextInt();
+		int n1=n;
+		int c=0;
+		int v=0;
+		while(n1!=0)
+		{
+			v=n1%10;
+			c+=n*v;
+			n1=n1/10;
+			
+		}
+		if(c==ns)
+		System.out.println("seed");
+		else
+			System.out.println("not seed");
+		s.close();
+	}
+	}
+
+
